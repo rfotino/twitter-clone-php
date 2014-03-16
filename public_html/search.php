c3b5226321bf98ab661091e2e4c8a63db98bc4f6
Robert Fotino <robert.fotino@gmail.com>
2014-03-16 06:46:34 -0700

    Added pagination for search results. Refs #7

--- public_html/search.php
+++ public_html/search.php
@@ -2,6 +2,7 @@
 
 define("WEBPAGE_CONTEXT", "edit-profile.php");
 define("INCLUDE_HEADER", true);
+define("RESULTS_PER_PAGE", 15);
 
 set_include_path(implode(PATH_SEPARATOR, array(
     __DIR__,
@@ -17,13 +18,44 @@ require_once("header.inc.php");
 ?>
 
 <div class="box">
-    <h2>Results for "<?php echo $user_query; ?>"</h2>
     <?php
+    
+    $count_query = "SELECT COUNT(*) AS `num_results` FROM `users`
+                    WHERE `handle` LIKE '%".$db->real_escape_string($user_query)."%'
+                    OR `name` LIKE '%".$db->real_escape_string($user_query)."%'
+                    OR `bio` LIKE '%".$db->real_escape_string($user_query)."%'";
+    $count_results = $db->query($count_query);
+    if ($count_results && $count_results->num_rows) {
+        $count_result_row = $count_results->fetch_assoc();
+        $num_results = (int)$count_result_row['num_results'];
+    } else {
+        $num_results = 0;
+    }
+    
+    if (isset($_GET['p']) && (int)$_GET['p']) {
+        $current_page = (int)$_GET['p'];
+    } else {
+        $current_page = 1;
+    }
+    
+    $last_page = ceil($num_results / RESULTS_PER_PAGE);
+    if ($current_page > $last_page) {
+        $current_page = $last_page;
+    } else if ($current_page < 1) {
+        $current_page = 1;
+    }
+    
+    ?>
+    
+    <h2>Results for "<?php echo $user_query; ?>"</h2>
+    
+    <?php
+    
     $query = "SELECT * FROM `users`
               WHERE `handle` LIKE '%".$db->real_escape_string($user_query)."%'
               OR `name` LIKE '%".$db->real_escape_string($user_query)."%'
               OR `bio` LIKE '%".$db->real_escape_string($user_query)."%'
-              LIMIT 15";
+              LIMIT ".(($current_page - 1) * RESULTS_PER_PAGE).", ".RESULTS_PER_PAGE;
     $results = $db->query($query);
     if ($results && $results->num_rows) {
         while ($row = $results->fetch_assoc()) {
@@ -32,7 +64,26 @@ require_once("header.inc.php");
     } else {
         echo "<p>No matching users found!</p>";
     }
+    
     ?>
+    
+    <div class="pagination">
+        <div class="prev-page">
+            <?php if ($current_page > 1) { ?>
+            <a href="<?php echo SITE_ROOT.DIRECTORY_SEPARATOR."search.php?q=".urlencode($_GET['q'])."&p=".($current_page - 1); ?>">&laquo; Previous</a>
+            <?php } ?>
+        </div><!--
+        
+     --><div class="curr-page">
+            <?php echo "Page $current_page"; ?>
+        </div><!--
+        
+     --><div class="next-page">
+            <?php if ($current_page < $last_page) { ?>
+            <a href="<?php echo SITE_ROOT.DIRECTORY_SEPARATOR."search.php?q=".urlencode($_GET['q'])."&p=".($current_page + 1); ?>">Next &raquo;</a>
+            <?php } ?>
+        </div>
+    </div>
 </div>
 
 <?php require_once("footer.inc.php"); ?>
