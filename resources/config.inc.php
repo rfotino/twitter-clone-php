d4e55a33818d83edf340ce66ebc2508ee4818a72
Robert Fotino <robert.fotino@gmail.com>
2014-03-26 03:06:40 -0700

    Committing updated config file.

--- resources/config.inc.php
+++ resources/config.inc.php
@@ -11,6 +11,7 @@ define("DB_PASS", "mL7FRKjawXqn9VqL");
 
 define("SITE_ROOT", "http://localhost/twitter-clone-php");
 define("BIO_MAX_LENGTH", 512);
+define("POST_MAX_LENGTH", 512);
 define("EMAIL_ENABLED", false);
 
 date_default_timezone_set("America/Los_Angeles");
